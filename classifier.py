02224a07c28f6a52ebcae70581b5b7297a34f031
bendsouza2 <bendsouza2@gmail.com>
2021-12-15 18:40:02 +0000

    plotted classification tree and confusion matrix

--- classifier.py
+++ classifier.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 
 df = pd.read_csv('EPL1819.csv', header=0)
 print(df.head())
@@ -9,7 +13,10 @@ unnecessary_columns = ['birthday',
                        'season',
                        'rank_in_league_top_defenders',
                        'rank_in_league_top_attackers',
-                       'rank_in_league_top_midfielders']
+                       'rank_in_league_top_midfielders',
+                       'full_name',
+                       'nationality',
+                       'Current Club']
 
 df_relevant = df.drop(unnecessary_columns, axis=1)
 
@@ -23,4 +30,19 @@ print(df_minsplayed.dtypes)
 X = df_minsplayed.drop('position', axis=1)  # data for classification
 y = df_minsplayed['position'].copy()  # columns to predict
 print(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+clf_dt = DecisionTreeClassifier()
+clf_dt = clf_dt.fit(X_train, y_train)
+
+
+plt.figure(figsize=(15, 7.5))
+plot_tree(clf_dt, filled=True, feature_names=df_minsplayed.columns)
+
+# class_names=['Goalkeeper', 'Forward', 'Midfielder', 'Defender'],
+
+cm = ConfusionMatrixDisplay.from_estimator(clf_dt, X_test, y_test)
+
+plt.show()
 
