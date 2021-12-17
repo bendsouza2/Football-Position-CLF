4f27fd1d6b1613a483064b4cc69207ad50883559
bendsouza2 <bendsouza2@gmail.com>
2021-12-17 19:22:20 +0000

    began cross-validation to find optimal value for alpha plotted grap of mean accuracy for different values of alpha

--- classifier.py
+++ classifier.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 
@@ -43,6 +44,22 @@ plot_tree(clf_dt, filled=True, feature_names=df_minsplayed.columns)
 # class_names=['Goalkeeper', 'Forward', 'Midfielder', 'Defender'],
 
 cm = ConfusionMatrixDisplay.from_estimator(clf_dt, X_test, y_test)
+
+# Cross validation and pruning
+path = clf_dt.cost_complexity_pruning_path(X_train, y_train)
+ccp_alphas = path.ccp_alphas
+ccp_alphas = ccp_alphas[:-1]  # removing max value
+
+# Optimal alpha with cross-validation
+alpha_loop_vals = []  # array to store results of each fold
+for alpha in ccp_alphas:
+    clf_dt = DecisionTreeClassifier(random_state=0, ccp_alpha=alpha)
+    scores = cross_val_score(clf_dt, X_train, y_train, cv=5)
+    alpha_loop_vals.append([alpha, np.mean(scores), np.std(scores)])
+
+# Plotting graph of accuracy scores
+alpha_results = pd.DataFrame(alpha_loop_vals, columns=['alpha', 'mean_accuracy', 'std'])
+alpha_results.plot(x='alpha', y='mean_accuracy', yerr='std', marker='o', linestyle='--')
 
 plt.show()
 
